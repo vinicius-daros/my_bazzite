be20cad8df56553d7f2bd181d14d578cc515aa27
vinicius-daros <viniciush.dev@gmail.com>
2024-12-10 20:06:17 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,11 +13,11 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+rpm-ostree override remove ptyxis
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
